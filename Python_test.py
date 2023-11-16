988b70d110a8abd59c773ad3c4b3f1611651d198
opilane <opilane@HP-CZC2349HT4>
2023-11-16 11:47:56 +0200

    Add project files.

---
+++ Python_test.py
@@ -0,0 +1,16 @@
+print("Hello")
+test="World"
+print(test)
+print("hello "+test)
+
+give0=str(input("give me a word: "))
+print(give0)
+
+give1=int(input("give a number: "))
+print(give1)
+
+print("Tere tulemust!")
+kool=input("Mis koolis sa õpid?: ")
+kursus=int(input("Mis kursusel?: "))
+print("Tere tuelmast kooli",kool,"\nOle hea ",str(kursus),".kuursese õpilaseks!")
+print("Tere tuelmast kooli",kool,"\nOle hea ",str(kursus),".kuursese õpilaseks!")
